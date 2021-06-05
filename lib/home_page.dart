35b00b29474aa917b9931c59d5226e82695dac6b
suriya chayatummagoon <suriya43426@gmail.com>
2021-06-05 23:33:35 +0700

    Initial commit

---
+++ lib/home_page.dart
@@ -0,0 +1,48 @@
+import 'package:apitest/utils.dart';
+import 'package:flutter/material.dart';
+
+import 'package:input_calculator/input_calculator.dart';
+
+class HomePage extends StatefulWidget {
+  static const id = 'home_page';
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double _value = 0.0;
+
+  set value(double value) {
+    setState(() {
+      _value = value;
+    });
+  }
+
+  String valueFormat(double? value) {
+    return CurrencyFormat.format(value!, symbol: '');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        padding: EdgeInsets.all(32.0),
+        child: CalculatorTextField(
+          initialValue: _value,
+          valueFormat: valueFormat,
+          theme: CalculatorThemes.curve,
+          inputDecoration: InputDecoration(
+            labelText: 'Value',
+            icon: Icon(Icons.attach_money),
+          ),
+          onSubmitted: (value) {
+            this.value = value!;
+            print('value: $value');
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils.dart
@@ -0,0 +1,16 @@
+import 'package:intl/intl.dart' show NumberFormat;
+
+class CurrencyFormat {
+  static String format(double value, {String? symbol}) {
+    if (value > 999999) {
+      return NumberFormat.compactSimpleCurrency(
+        name: symbol,
+        decimalDigits: 2,
+      ).format(value);
+    }
+    return NumberFormat.simpleCurrency(
+      name: symbol,
+      decimalDigits: 2,
+    ).format(value);
+  }
+}
